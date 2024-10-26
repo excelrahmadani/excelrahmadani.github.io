75ca8e1e087ad1989cd396ec92c072fcea09ac43
Excel Rahmadani <excelrahmadani51@gmail.com>
2024-10-27 02:16:16 +0700

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Tombol telah diklik!");
+});
+document.getElementById("myButton").addEventListener("click", function() {
+    window.open("https://www.instagram.com/exclrhmdhni_/", "_blank");
+});
